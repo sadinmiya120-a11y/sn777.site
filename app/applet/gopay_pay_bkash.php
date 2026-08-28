92eb50e37b22e44e9759a8a824171428cbe2aac5
sadinmiya120-a11y <sadinmiya120@gmail.com>
2026-08-28 08:29:36 +0600

    আপডেট

--- app/applet/gopay_pay_bkash.php
+++ app/applet/gopay_pay_bkash.php
@@ -5,19 +5,25 @@ include("../serive/samparka.php");
 
 if (!isset($_GET['uid']) || !isset($_GET['amount'])) { die("Illegal access: UID or Amount missing"); }
 
-$uid   = mysqli_real_escape_string($conn, $_GET['uid']);
+$uid   = isset($conn) && $conn ? mysqli_real_escape_string($conn, $_GET['uid']) : htmlspecialchars($_GET['uid']);
 $ramt  = (int)$_GET['amount']; 
 $payName = 'BKASH';
-$serial = isset($_GET['order_no']) && !empty($_GET['order_no']) ? mysqli_real_escape_string($conn, $_GET['order_no']) : (date("Ymd").time().rand(100000,999999));
+$serial = isset($_GET['order_no']) && !empty($_GET['order_no']) ? (isset($conn) && $conn ? mysqli_real_escape_string($conn, $_GET['order_no']) : htmlspecialchars($_GET['order_no'])) : (date("Ymd").time().rand(100000,999999));
 $createdate = date("Y-m-d H:i:s");
 
 // ১. ডাটাবেজ থেকে মোবাইল নম্বর রিড করা
-$q = mysqli_query($conn, "SELECT mobile FROM shonu_subjects WHERE id='$uid'");
-$u = mysqli_fetch_array($q);
-$mobile = isset($u['mobile']) ? $u['mobile'] : '01700000000';
+$mobile = '01700000000';
+if (isset($conn) && $conn) {
+    $q = mysqli_query($conn, "SELECT mobile FROM shonu_subjects WHERE id='$uid'");
+    if ($q && $u = mysqli_fetch_array($q)) {
+        $mobile = isset($u['mobile']) && !empty($u['mobile']) ? $u['mobile'] : '01700000000';
+    }
+}
 
 // ২. ইউজারের সিস্টেমে পেন্ডিং রেকর্ড ইনসার্ট
-mysqli_query($conn, "INSERT INTO thevani (payid, balakedara, motta, dharavahi, mula, ullekha, duravani, ekikrtapavati, dinankavannuracisi, madari, pavatiaidi, sthiti) VALUES ('2', '$uid', '$ramt', '$serial', '$payName', 'N/A', '$mobile', 'BKASH', '$createdate', '1005', '2', '0')");
+if (isset($conn) && $conn) {
+    mysqli_query($conn, "INSERT INTO thevani (payid, balakedara, motta, dharavahi, mula, ullekha, duravani, ekikrtapavati, dinankavannuracisi, madari, pavatiaidi, sthiti) VALUES ('2', '$uid', '$ramt', '$serial', '$payName', 'N/A', '$mobile', 'BKASH', '$createdate', '1005', '2', '0')");
+}
 
 /* ================= GOPAY API CONFIG ================= */
 $config = require 'gopayconfig.php';
@@ -65,7 +71,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
 $response = curl_exec($ch);
-curl_close($ch); // সঠিক curl_close করা হয়েছে
+curl_close($ch);
 
 $res = json_decode($response, true);
 
