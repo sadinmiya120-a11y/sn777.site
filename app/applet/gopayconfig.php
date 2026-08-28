e54628b45b9859ee990d2335f0b4e0ad18d993f5
sadinmiya120-a11y <sadinmiya120@gmail.com>
2026-08-28 06:31:47 +0600

    Update

--- app/applet/gopayconfig.php
+++ app/applet/gopayconfig.php
@@ -1,10 +1,10 @@
 <?php
-// gopayconfig.php - GOPay Client Configuration File
+// gopayconfig.php - gopay Client Configuration File
 return [
     'app_id'     => 'GP_97386700', // এডমিন থেকে পাওয়া App ID
     'secret_key' => '87a89555480aae027ad84daf666602d7', // এডমিন থেকে পাওয়া Secret Key
     
     // আপনার গেটওয়ের মূল এপিআই রাউটার লিংক
-    'api_url'    => 'https://mch.go-pay.cyou/pay.php'
+    'api_url'    => 'https://mch.go-pay.cyou/pay.php' 
 ];
 ?>
